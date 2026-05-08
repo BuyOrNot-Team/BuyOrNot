a01bc1400065af067c9f26476b088ebe77a7e085
kwonsj0825 <kwonsj0825@naver.com>
2026-05-08 16:56:22 +0900

    init: initial project setup

---
+++ src/test/java/com/basic/buyornot/BuyOrNotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.basic.buyornot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BuyOrNotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
